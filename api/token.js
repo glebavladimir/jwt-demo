4ee7ae4cacc79b9206f273a13c691865594c06d3
Vladimir Gleba <gleba.vladimir@gmail.com>
2021-12-22 00:24:56 +0200

    Refactor else-if construction to switch

--- api/token.js
+++ api/token.js
@@ -19,49 +19,52 @@ async function handler(request, reply) {
     );
     console.info('[db] Mongoose is successfully connected')
 
-    if (request.method === 'POST') {
-        const token = request.body.token;
-        if (token == null) {
-            reply.status(400).send({ message: "Token is required" })
-            return
-        }
-        const refreshToken = await RefreshToken.findOne({token: token}).exec()
-        if (!refreshToken || !refreshToken.token) {
-            reply.code(403).send({"error": "Invalid token"})
-            return
-        }
-
-        jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (error, jwtUser) => {
-            if (error) {
-                reply.status(403).send(error)
+    switch(request.method) {
+        case 'POST':
+            const token = request.body.token;
+            if (token == null) {
+                reply.status(400).send({ message: "Token is required" })
                 return
             }
-            const accessToken = jwt.sign(
-                { name: jwtUser.name }, 
-                process.env.ACCESS_TOKEN_SECRET, 
-                { expiresIn: '15s' }
-            )
-            reply.status(200).send({
-                accessToken: accessToken
-            });
-        });
-    } else if (request.method === 'DELETE') {
-        const token = request.body.token;
-        if (token === null) {
-            reply.status(400).send({ message: "Token is required" })
-            return
-        }
+            const refreshToken = await RefreshToken.findOne({token: token}).exec()
+            if (!refreshToken || !refreshToken.token) {
+                reply.code(403).send({"error": "Invalid token"})
+                return
+            }
 
-        const refreshToken = await RefreshToken.findOneAndDelete({ token: token }).exec();
-        if (!refreshToken || !refreshToken.token) {
-            reply.status(403).send({"error": "Invalid token"})
+            jwt.verify(token, process.env.REFRESH_TOKEN_SECRET, (error, jwtUser) => {
+                if (error) {
+                    reply.status(403).send(error)
+                    return
+                }
+                const accessToken = jwt.sign(
+                    { name: jwtUser.name }, 
+                    process.env.ACCESS_TOKEN_SECRET, 
+                    { expiresIn: '15s' }
+                )
+                reply.status(200).send({
+                    accessToken: accessToken
+                });
+                return
+            });
+        case 'DELETE':
+            const token = request.body.token;
+            if (token === null) {
+                reply.status(400).send({ message: "Token is required" })
+                return
+            }
+
+            const refreshToken = await RefreshToken.findOneAndDelete({ token: token }).exec();
+            if (!refreshToken || !refreshToken.token) {
+                reply.status(403).send({"error": "Invalid token"})
+                return
+            }
+            
+            reply.status(204)
             return
-        }
-        
-        reply.status(204)
-    } else {
-        reply.status(405).send({ message: 'Only POST and DELETE requests allowed' })
-        return
+        default:
+            reply.status(405).send({ message: 'Only POST and DELETE requests allowed' })
+            return
     }
 }
 
